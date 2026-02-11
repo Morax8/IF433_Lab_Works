e9c37a1e95069939948708b8a5a57ea53df9eb04
Aditya Zianur <adityazianurrahman@gmail.com>
2026-02-11 15:54:29 +0700

    fixing : week02: challenge refactor primary constructor using default args

--- src/oop_128409_AdityaZianur/week02/Student.kt
+++ src/oop_128409_AdityaZianur/week02/Student.kt
@@ -3,8 +3,8 @@ package oop_128409_AdityaZianur.week02
 class Student(
     val name: String,
     val nim:String,
-    val gpa: Double,
-    val major:String,
+    var major:String,
+    var gpa: Double = 0.0
 ){
     init {
         if (nim.length != 5){
