9bda58154947e09995bf1f97598b0f10f426ed1b
Aditya Zianur <adityazianurrahman@gmail.com>
2026-02-18 15:41:37 +0700

    small fix on week02: (task 2) implement the game

--- src/oop_128409_AdityaZianur/week02/Hero.kt
+++ src/oop_128409_AdityaZianur/week02/Hero.kt
@@ -2,6 +2,29 @@ package oop_128409_AdityaZianur.week02
 
 import java.util.Scanner
 
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+
+    fun attack(targetName: String) {
+        println("$name menebas $targetName dengan damage $baseDamage!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) {
+            hp = 0
+        }
+        println("$name terkena damage $damage. Sisa HP: $hp")
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
+
 fun main() {
     val sc = Scanner(System.`in`)
     val enemyName = "The Void King"
