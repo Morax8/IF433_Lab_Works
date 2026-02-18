edbebb6ffdbc1474580ddf77d683c3e55ddfa83f
Aditya Zianur <adityazianurrahman@gmail.com>
2026-02-18 15:31:07 +0700

    week03: test all encapsulation logic in main

--- src/oop_128409_AdityaZianur/week03/Main.kt
+++ src/oop_128409_AdityaZianur/week03/Main.kt
@@ -2,6 +2,12 @@ package oop_128409_AdityaZianur.week03
 
 fun main(){
     val e = Employee("Budi")
-    println("tes error")
-    e.salary = 5000
+
+   e.salary = -1000
+   e.salary = 5000000
+   println("Gaji: ${e.salary}")
+
+   e.increasePerformance()
+
+   println("Pajak yang harus dibayar: ${e.tax}")
 }
