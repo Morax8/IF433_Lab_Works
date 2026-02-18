36d0c76c33551328aa970044c049cc2646a39d8d
Aditya Zianur <adityazianurrahman@gmail.com>
2026-02-18 15:34:05 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_128409_AdityaZianur/week03/Main.kt
+++ src/oop_128409_AdityaZianur/week03/Main.kt
@@ -1,14 +1,25 @@
 package oop_128409_AdityaZianur.week03
 
 fun main(){
-    val e = Employee("Budi")
+//    val e = Employee("Budi")
+//
+//   e.salary = -1000
+//   e.salary = 5000000
+//   println("Gaji: ${e.salary}")
+//
+//   e.increasePerformance()
+//
+//
+//   println("Pajak yang harus dibayar: ${e.tax}")
 
-   e.salary = -1000
-   e.salary = 5000000
-   println("Gaji: ${e.salary}")
+    val wp = Weapon("Axe of Destiny")
 
-   e.increasePerformance()
+    // 1. Coba set damage ke -50 (Harus gagal/tetap nilai lama)
+    wp.damage = -50
 
+    // 2. Set damage ke 9999 (Harus terpaksa jadi 1000)
+    wp.damage = 9999
 
-   println("Pajak yang harus dibayar: ${e.tax}")
+    // 3. Print Tier-nya
+    println("Tier senjata saat ini: ${wp.tier}")
 }
