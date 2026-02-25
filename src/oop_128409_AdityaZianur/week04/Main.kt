c28d832916661299e455f5ac362ddb9c30e39ed2
Aditya Zianur <adityazianurrahman@gmail.com>
2026-02-25 15:18:43 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_128409_AdityaZianur/week04/Main.kt
+++ src/oop_128409_AdityaZianur/week04/Main.kt
@@ -1,22 +1,28 @@
 package oop_128409_AdityaZianur.week04
 
 fun main() {
-    println("--- TESTING VEHICLE ---")
-    val generalVehicle = Vehicle("Sepeda Listrik")
-    generalVehicle.honk()
-    generalVehicle.accelerate()
+//    println("--- TESTING VEHICLE ---")
+//    val generalVehicle = Vehicle("Sepeda Listrik")
+//    generalVehicle.honk()
+//    generalVehicle.accelerate()
+//
+//    println("\n--- TESTING CAR2 ---")
+//    val myCar = Car("Toyota", 4)
+//    myCar.openTrunk()
+//    myCar.honk()
+//    myCar.accelerate()
+//
+//    println("\n--- TESTING E-Car ---")
+//    val Ecar = ElectricCar("Tesla", 4, 80)
+//    Ecar.accelerate()
+//    Ecar.honk()
+//    Ecar.openTrunk()
 
-    println("\n--- TESTING CAR2 ---")
-    val myCar = Car("Toyota", 4)
-    myCar.openTrunk()
-    myCar.honk()
-    myCar.accelerate()
+    val Mng = Manager("Tono", 50000000)
+    Mng.work()
+    println("Bonus Tono: Rp ${Mng.calculateBonus()}")
 
-    println("\n--- TESTING E-Car ---")
-    val Ecar = ElectricCar("Tesla", 4, 80)
-    Ecar.accelerate()
-    Ecar.honk()
-    Ecar.openTrunk()
-
-
+    val dev = Developer("Jono", 20000000, "Kotlin")
+    dev.work()
+    println("Bonus Jono: Rp ${dev.calculateBonus()}")
 }
