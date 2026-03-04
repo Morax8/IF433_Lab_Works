dbd2e5d2d5f8f85934d2826ce8014162636ffa79
Aditya Zianur <adityazianurrahman@gmail.com>
2026-03-04 15:15:22 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_128409_AdityaZianur/week05/Tugas2Main.kt
@@ -0,0 +1,14 @@
+package oop_128409_AdityaZianur.week05
+
+fun main() {
+    val eWallet = EWallet("EWallet Zian", 50000.0)
+    val creditCard = CreditCard("Credit Card Zian", 100000.0)
+
+    val paymentMethods: List<PaymentMethod> = listOf(eWallet, creditCard)
+
+    for (method in paymentMethods) {
+        println("Memproses pembayaran untuk: ${method.accountName}")
+        method.processPayment(75000.0)
+        println("")
+    }
+}
