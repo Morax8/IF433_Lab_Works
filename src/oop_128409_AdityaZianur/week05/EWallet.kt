b9a719ef1641221be0036d4a45f5c7a8a304879e
Aditya Zianur <adityazianurrahman@gmail.com>
2026-03-04 15:09:54 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_128409_AdityaZianur/week05/EWallet.kt
@@ -0,0 +1,18 @@
+package oop_128409_AdityaZianur.week05
+
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran berhasil. Saldo tersisa: $balance")
+        } else {
+            println("Saldo tidak cukup")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top up berhasil. Saldo saat ini: $balance")
+    }
+}
