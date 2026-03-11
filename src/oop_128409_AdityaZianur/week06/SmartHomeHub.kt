50a6ce6b6abf906795714a2cc234fa4d133ff9ab
Aditya Zianur <adityazianurrahman@gmail.com>
2026-03-11 15:39:40 +0700

    "week06: (task) create SmartHomeHub logic"

---
+++ src/oop_128409_AdityaZianur/week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_128409_AdityaZianur.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
