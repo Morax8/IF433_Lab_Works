114cffcafba9c77a3b081765c86ab599ce434e35
Aditya Zianur <adityazianurrahman@gmail.com>
2026-03-11 15:45:38 +0700

    "week06: (task) instantiate smart devices in main"

--- src/oop_128409_AdityaZianur/week06/Main.kt
+++ src/oop_128409_AdityaZianur/week06/Main.kt
@@ -1,21 +1,26 @@
 package oop_128409_AdityaZianur.week06
 
-fun processCheckout(method: PaymentMethod, amount: Double){
-    println("-> Memulai Checkout...")
-    method.pay(amount)
-}
+//fun processCheckout(method: PaymentMethod, amount: Double){
+//    println("-> Memulai Checkout...")
+//    method.pay(amount)
+//}
 
 fun main(){
-    val myWatch = Smartwatch()
-    myWatch.showTime()
+    val lampu = SmartLamp("L01", "Ruang Tamu")
+    val speaker = SmartSpeaker("S01", "Google Nest Dapur")
+    val cctv = SmartCCTV("C01", "Ezviz Garasi")
 
-    val myPhone = Smartphoe()
-    myPhone.turnOn()
+//    val myWatch = Smartwatch()
+//    myWatch.showTime()
+//
+//    val myPhone = Smartphoe()
+//    myPhone.turnOn()
+//
+//    val pay1 = Gopay()
+//    val pay2 = CreditCard()
+//
+//    println("\n=== Testing Checkout ===")
+//    processCheckout(pay1, 50000.0)
+//    processCheckout(pay2, 150000.0)
 
-    val pay1 = Gopay()
-    val pay2 = CreditCard()
-
-    println("\n=== Testing Checkout ===")
-    processCheckout(pay1, 50000.0)
-    processCheckout(pay2, 150000.0)
 }
