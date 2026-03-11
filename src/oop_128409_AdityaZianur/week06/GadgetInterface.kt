73fe5d5cb40950bcc5d1bc38594548cc425468bd
Aditya Zianur <adityazianurrahman@gmail.com>
2026-03-11 15:13:22 +0700

    "week06: create Camera and Phone interfaces with default methods"

---
+++ src/oop_128409_AdityaZianur/week06/GadgetInterface.kt
@@ -0,0 +1,9 @@
+package oop_128409_AdityaZianur.week06
+
+interface Camera{
+    fun turnOn(){ println("Lensa terbuka dan sensor aktif")}
+}
+
+interface Phone{
+    fun turnOn(){println("Sinyal seluler mencar jaringan")}
+}
