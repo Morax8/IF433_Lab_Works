541fde8235876abd119509b6df4ba1af4481bd99
Aditya Zianur <adityazianurrahman@gmail.com>
2026-03-11 15:14:48 +0700

    "week06: fix diamond problem using explicit super delegation"

--- src/oop_128409_AdityaZianur/week06/Smartphoe.kt
+++ src/oop_128409_AdityaZianur/week06/Smartphoe.kt
@@ -1,4 +1,9 @@
 package oop_128409_AdityaZianur.week06
 
 class Smartphoe: Camera, Phone {
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+        println("Sistem operasi smartphone berhasil booting")
+    }
 }
