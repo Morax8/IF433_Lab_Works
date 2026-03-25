a3e145fe8c9bd4f93f4758897a0f6a9dae0318e8
Aditya Zianur <adityazianurrahman@gmail.com>
2026-03-25 15:39:23 +0700

    "week07: (trial) trigger non-exhaustive when compiler error"

--- src/oop_128409_AdityaZianur/week07/SystemStates.kt
+++ src/oop_128409_AdityaZianur/week07/SystemStates.kt
@@ -4,6 +4,6 @@ enum class AppStates {STARTING, RUNNING, STOPPED}
 
 sealed class ApiResponse {
     data class Success(val data: String) : ApiResponse()
-    data class Failure(val message: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
     object Loading : ApiResponse()
 }
