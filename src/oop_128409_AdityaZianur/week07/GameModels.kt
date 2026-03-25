2a774352c1704f072efb3014034ca2df9c1c9227
Aditya Zianur <adityazianurrahman@gmail.com>
2026-03-25 15:47:38 +0700

    "week07: (task) create GameItem data class" + ubah rarity jadi int(salah paham tadi)

--- src/oop_128409_AdityaZianur/week07/GameModels.kt
+++ src/oop_128409_AdityaZianur/week07/GameModels.kt
@@ -1,9 +1,16 @@
 package oop_128409_AdityaZianur.week07
 
-enum class ItemRarity(val chance: Double) {
-    COMMON(0.70),
-    UNCOMMON(0.55),
-    RARE(0.25),
-    EPIC(0.04),
-    LEGENDARY(0.01)
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(40),
+    RARE(20),
+    EPIC(5),
+    LEGENDARY(1)
 }
+
+// Checkpoint 14: Data Class Item Game
+data class GameItem(
+    val name: String,
+    val damage: Int,
+    val rarity: ItemRarity
+)
