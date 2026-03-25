704bd871a682fefbd3baef476aab21c4c4a5f1d6
Aditya Zianur <adityazianurrahman@gmail.com>
2026-03-25 15:52:25 +0700

    "week07: (task) create BattleState sealed class hierarchy"

---
+++ src/oop_128409_AdityaZianur/week07/BattleEvent.kt
@@ -0,0 +1,8 @@
+package oop_128409_AdityaZianur.week07
+
+sealed class BattleState {
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+    data class LootDropped(val item: GameItem) : BattleState()
+    data class GameOver(val reason: String) : BattleState()
+    object SafeZone : BattleState()
+}
