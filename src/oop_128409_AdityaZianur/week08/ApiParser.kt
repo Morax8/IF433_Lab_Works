ca3da4fdf102794e81f9c34f99ce760c79fae038
Aditya Zianur <adityazianurrahman@gmail.com>
2026-04-15 14:22:53 +0700

    "week08: (task) implement robust ApiParser using Elvis and requireNotNull"

---
+++ src/oop_128409_AdityaZianur/week08/ApiParser.kt
@@ -0,0 +1,21 @@
+package oop_128409_AdityaZianur.week08
+
+class ApiParser {
+    fun parseProduct(rawJson: Map<String, Any?>): Product? {
+        val id = requireNotNull(rawJson["id"] as? String) { "API Invalid: Missing ID" }
+        val name = requireNotNull(rawJson["name"] as? String) { "API Invalid: Missing Name" }
+        val type = rawJson["type"] as? String
+
+        if (type == "ELECTRONIC") {
+            val warranty = rawJson["warranty"] as? Int ?: 12
+            return Electronic(id, name, warranty)
+        }
+
+        if (type == "CLOTHING") {
+            val size = rawJson["size"] as? String ?: "All Size"
+            return Clothing(id, name, size)
+        }
+
+        return null
+    }
+}
