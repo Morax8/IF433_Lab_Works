bd13dfa62c4cb350d2abb51d0fae877b84fc85e0
Aditya Zianur <adityazianurrahman@gmail.com>
2026-04-15 14:13:57 +0700

    "week08: implement chained safe calls and elvis"

---
+++ src/oop_128409_AdityaZianur/week08/Main.kt
@@ -0,0 +1,8 @@
+package oop_128409_AdityaZianur.week08
+
+fun main() {
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
+    println("Tujuan pengiriman: $destination")
+}
