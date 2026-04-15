066b4674b1165d65ce2035391cd1ebec2dd0da84
Aditya Zianur <adityazianurrahman@gmail.com>
2026-04-15 14:23:25 +0700

    "week08: (task) create JavaPaymentService for payment interop"

---
+++ src/oop_128409_AdityaZianur/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_128409_AdityaZianur.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
