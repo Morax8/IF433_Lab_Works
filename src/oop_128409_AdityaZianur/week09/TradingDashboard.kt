bd5ba0217f9de67001a448f9419103403bf6f0ac
Aditya Zianur <adityazianurrahman@gmail.com>
2026-04-22 15:31:21 +0700

    "week09: (task) populate mock trade history list"

---
+++ src/oop_128409_AdityaZianur/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_128409_AdityaZianur.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 50, 45.0, "OPEN"),
+        TradeLog("SOLUSDT", "LONG", 15, 10.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 20, -12.0, "OPEN"),
+        TradeLog("BNBUSDT", "SHORT", 5, -2.5, "CLOSED")
+    )
+}
