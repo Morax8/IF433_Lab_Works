ce91017c30803efc1d7304e4a36dfc69ae1753b9
Aditya Zianur <adityazianurrahman@gmail.com>
2026-05-13 15:34:55 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_128409_AdityaZianur/week12/SmartFeeder.kt
+++ src/oop_128409_AdityaZianur/week12/SmartFeeder.kt
@@ -14,4 +14,15 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    println("=== JADWAL MAKAN PAGI ===")
+    try {
+        currentKibbleStock = dispenseKibble(requestedGram = 80, availableGram = currentKibbleStock, isJammed = false)
+    } catch (e: DispenserJamException) {
+        println("Peringatan ke Pemilik: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Peringatan ke Pemilik: ${e.message}")
+    } catch (e: Exception) {
+        println("Peringatan ke Pemilik: Terjadi kesalahan tidak terduga. ${e.message}")
+    }
 }
