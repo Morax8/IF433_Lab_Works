8a728eb352639d53a854cb8feb4be65a906da4cd
Aditya Zianur <adityazianurrahman@gmail.com>
2026-05-20 10:57:53 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_128409_AdityaZianur/week13/CryptoTradeLogger.kt
@@ -0,0 +1,6 @@
+package oop_128409_AdityaZianur.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
